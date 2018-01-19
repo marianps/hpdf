abec10a651e6d1dcdaba73ea2077e60f8cfeb421
marianps <pritham.saldanha@gmail.com>
2018-01-19 15:01:27 +0530

    Adding authetication to app

--- microservices/t47d/src/server.py
+++ microservices/t47d/src/server.py
@@ -1,6 +1,6 @@
 from src import app
-from flask import jsonify
-
+import requests
+from flask import jsonify, render_template, request, make_response,json
 
 @app.route("/")
 def home():
@@ -11,3 +11,72 @@ def home():
 @app.route("/json")
 def json_message():
     return jsonify(message="Hello World")
+
+@app.route("/index")
+def index():
+    return render_template('dlogin.html')
+
+@app.route("/dlogin", methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        vuser = request.form['vName']
+        vpwd = request.form['vPwd']
+    # This is the url to which the query is made
+    url = "https://auth.anthology78.hasura-app.io/v1/login"
+
+    # This is the json payload for the query
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": vuser,
+            "password": vpwd
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+    print(requestPayload)
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+    # resp.content contains the json response.
+
+    print(resp.content)
+    return render_template('homedrive.html')
+
+
+@app.route("/dregister", methods = ['POST', 'GET'])
+def dregister():
+
+    # This is the url to which the query is made
+    url = "https://auth.anthology78.hasura-app.io/v1/signup"
+
+    vuser = request.form['vName']
+    vpwd = request.form['vPwd']
+    # This is the json payload for the query
+    requestPayload = {
+    "provider": "username",
+    "data": {
+        "username": vuser,
+        "password": vpwd
+            }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+    #  resp.content contains the json response.
+    print (resp.content)
+    return render_template('dlogin.html')
+
+# Handling all other request and robots.txt request
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
