a22e55415ca27a06daaf69d19613ff78ec386998
marianps <pritham.saldanha@gmail.com>
2018-01-26 17:31:36 +0530

    fixing sending int val to cookie

--- microservices/t47d/src/server.py
+++ microservices/t47d/src/server.py
@@ -271,7 +271,7 @@ def dlogin():
             respo = make_response(resp.content)
             respo.set_cookie(CLUSTER_NAME, vauthdata['auth_token'])
             respo.set_cookie(vauthdata['auth_token'], vauthdata['username'])
-            respo.set_cookie('rtpthid', usrdt[0]['root_path_id'])
+            respo.set_cookie('rtpthid', str(usrdt[0]['root_path_id']))
 
         else:
             fldrresp=r_folderlist(vauthdata['auth_token'],vauthdata['hasura_id'],usrdt[0]['root_path_id'])
@@ -279,7 +279,7 @@ def dlogin():
             respo = make_response(render_template('homedrive.html', name=vauthdata['username'], msg=resp.content, response1=fldrresp.content + flresp.content))
             respo.set_cookie(CLUSTER_NAME, vauthdata['auth_token'])
             respo.set_cookie(vauthdata['auth_token'], vauthdata['username'])
-            respo.set_cookie('rtpthid', usrdt[0]['root_path_id'])
+            respo.set_cookie('rtpthid', str(usrdt[0]['root_path_id']))
 
         return respo
     else:
