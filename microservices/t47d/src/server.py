9e3281a685a3aa2abfa1570fbf720e34e93ccf84
marianps <pritham.saldanha@gmail.com>
2018-01-26 19:12:06 +0530

    typecast issue

--- microservices/t47d/src/server.py
+++ microservices/t47d/src/server.py
@@ -463,12 +463,12 @@ def dregister():
                         respo = make_response(resp.content)
                         respo.set_cookie(CLUSTER_NAME, vauthdata['auth_token'])
                         respo.set_cookie(vauthdata['auth_token'], vauthdata['username'])
-                        respo.set_cookie('rtpthid', fldrid)
+                        respo.set_cookie('rtpthid', str(fldrid))
                     else:
                         respo = make_response(render_template('homedrive.html', name=vauthdata['username'], msg=resp.content, response1=cpthrep.content + rtfldr.content+cusrrep.content))
                         respo.set_cookie(CLUSTER_NAME, vauthdata['auth_token'])
                         respo.set_cookie(vauthdata['auth_token'], vauthdata['username'])
-                        respo.set_cookie('rtpthid', fldrid)
+                        respo.set_cookie('rtpthid', str(fldrid))
 
                     return respo
                 #Failure of insert into App_user
